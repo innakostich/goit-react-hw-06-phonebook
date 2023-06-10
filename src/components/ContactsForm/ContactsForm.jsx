26bccc36edfb47f62e1c9d3d4788a5fe28d535f0
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-06-10 17:27:05 +0200

    adds redux packages

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,58 +1,3 @@
-// import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
-// import css from './ContactsForm.module.css';
-
-// const ContactsForm = ({ onAddContact }) => {
-//   const [name, setName] = useState('');
-//   const [number, setNumber] = useState('');
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     if (name.trim() === '' || number.trim() === '') {
-//       return;
-//     }
-//     const formattedName = formatName(name.trim());
-//     onAddContact(formattedName, number);
-//     setName('');
-//     setNumber('');
-//   };
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-//     if (name === 'name') {
-//       setName(value);
-//     } else if (name === 'number') {
-//       setNumber(value);
-//     }
-//   };
-
-//   const formatName = (name) => {
-//     const [firstName, ...lastName] = name.split(' ');
-//     const formattedFirstName = capitalizeFirstLetter(firstName.toLowerCase());
-//     const formattedLastName = lastName.map((part) => capitalizeFirstLetter(part.toLowerCase()));
-//     return [formattedFirstName, ...formattedLastName].join(' ');
-//   };
-
-//   const capitalizeFirstLetter = (str) => {
-//     return str.charAt(0).toUpperCase() + str.slice(1);
-//   };
-
-//   return (
-//     <form className={css.contactsForm} onSubmit={handleSubmit}>
-//       <label>Name</label>
-//       <input type="text" name="name" placeholder="Enter name" value={name} onChange={handleChange} />
-//       <label>Phone Number</label>
-//       <input type="text" name="number" placeholder="Phone Number" value={number} onChange={handleChange} />
-//       <button type="submit">Add contact</button>
-//     </form>
-//   );
-// };
-
-// ContactsForm.propTypes = {
-//   onAddContact: PropTypes.func.isRequired,
-// };
-
-// export default ContactsForm;
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactsForm.module.css';
@@ -108,3 +53,4 @@ ContactsForm.propTypes = {
 };
 
 export default ContactsForm;
+
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -28,3 +28,33 @@ ContactsList.propTypes = {
 };
 
 export default ContactsList;
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { deleteContact } from 'redux/contactsSlice';
+
+// import css from './ContactsList.module.css';
+
+// const ContactsList = () => {
+//   const contacts = useSelector((state) => state.contacts.items);
+//   const dispatch = useDispatch();
+
+//   const handleDeleteContact = (id) => {
+//     dispatch(deleteContact(id));
+//   };
+
+//   return (
+//     <ul className={css.list}>
+//     {contacts.map((contact) => (
+//       <li key={contact.id} className={css.item}>
+//         <p className={css.name}>{contact.name}</p>
+//         <p className={css.number}>{contact.number}</p>
+//         <button className={css.deleteButton} onClick={() => handleDeleteContact(contact.id)}>
+//             Delete
+//         </button>
+//       </li>
+//     ))}
+//   </ul>
+//   );
+// };
+
+// export default ContactsList;
